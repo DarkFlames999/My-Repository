91a42b9b3a7bbe0297a37a0307fc833ddf3731ab
Isaiah Watkins <itwatkins2007@gmail.com>
2025-10-27 10:44:26 -0700

    New changes merged

--- main.cpp
+++ main.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+void say(string s);
+
 int main()
 {
     cout << "Greetings...\n";
-    cout << "Hello World!\n";        
+    cout << "Hello World!\n";   
+    
+    say("Hi");
+    
     return 0;
+}
+
+void say(string s)
+{
+    cout << s << endl;
 }
